db03e3b923920ca5c17d924b89db9998a09a9a92
Vailton Gomes <governador.adm@gmail.com>
2020-05-29 21:10:17 -0300

    Exercício Angolar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+  textoP1: string = 'primeiro paragrafo'
+  mostarP1: boolean = true
+  itens: number[] = [2, 4, 6, 8, 10]
 
   constructor() { }
 
